26b4788a7eb74dd30217f9f39771c7905a0b43dd
jaewoong-87 <mojjom@naver.com>
2024-04-08 16:13:18 +0900

    clean push

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  env: {
+    API_URL:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api" // 개발 환경일 때 API URL
+        : "https://chat-pkts.vercel.app/api", // 프로덕션 환경일 때 API URL
+  },
 };
 
 module.exports = nextConfig;
