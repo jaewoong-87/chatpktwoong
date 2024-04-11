7af5dbcf96efef9e43cec40a05da5ec6f2872e0a
jaewoong woo <64830455+jaewoong-87@users.noreply.github.com>
2024-04-11 17:35:31 +0900

    Update chat.js

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,30 +1,36 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from "axios";
+import Cors from 'cors';
+import axios from 'axios';
+import initMiddleware from '../../lib/init-middleware';
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  Cors({
+    // Only allow requests with POST and from specific origins
+    methods: ['POST'],
+    origin: true, // Reflect the request origin, as defined by the spec OR set specific origins
+  })
+);
 
 export default async function handler(req, res) {
-  const referer = req.headers.referer || req.headers.referrer; // get the referer from the request headers
+  // Run cors
+  await cors(req, res);
 
   if (req.method !== "POST") {
-    res.status(405).json({ message: "Method should be POST" });
-  } else if (process.env.VERCEL_ENV !== "development") {
-    if (!referer || referer !== process.env.APP_URL) {
-      res.status(401).json({ message: "Unauthorized" });
-    }
-  } else {
-    try {
-      const { body } = req;
-      const url = "https://api.openai.com/v1/chat/completions";
-      const headers = {
-        "Content-type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      };
+    return res.status(405).json({ message: "Method should be POST" });
+  }
 
-      const response = await axios.post(url, body, { headers: headers });
+  try {
+    const { body } = req;
+    const url = "https://api.openai.com/v1/chat/completions";
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    };
 
-      res.status(200).json(response.data);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Something went wrong" });
-    }
+    const response = await axios.post(url, body, { headers: headers });
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(error.response.status || 500).json({ message: "Something went wrong" });
   }
 }
