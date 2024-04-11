512e53f258cab1026eef57318c07fcbef6aa6cf4
jaewoong-87 <mojjom@naver.com>
2024-04-11 11:50:42 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  env: {
-    API_URL:
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/api" // 개발 환경일 때 API URL
-        : "https://chat-pkts.vercel.app/api", // 프로덕션 환경일 때 API URL
-  },
 };
 
 module.exports = nextConfig;
